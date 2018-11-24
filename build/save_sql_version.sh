207176f933636e3e454c29260beac5a400d099bd
Marten Seemann <martenseemann@gmail.com>
2018-11-24 20:10:02 +0700

    fix SQL build script

--- build/save_sql_version.sh
+++ build/save_sql_version.sh
@@ -7,7 +7,8 @@ CURRENTFILE=`ls -1 $DIR/../sql/update*.sql | xargs -n 1 basename | sort | tail -
 VERSION=`echo $CURRENTFILE | sed "s/update-v.*-to-v//g" | sed "s/\.sql//g"`
 echo "Found most recent SQL install version: $VERSION"
 
-CONFIGKEY=`grep "sConfigKey" $DIR/../core/oxconfk.php | awk -F"\"" '{print $2}'`
+# taken from vendor/oxid-esales/oxideshop-ce/source/Core/Config.php
+CONFIGKEY="fq45QS09_fqyx09239QQ"
 
 # the SQL command to write the SQL version to the database
 COMMAND="UPDATE oxconfig SET OXVARVALUE=ENCODE('$VERSION', '$CONFIGKEY') WHERE OXMODULE='theme:roxid' AND OXVARNAME='sqlVersion';"
